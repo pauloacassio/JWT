8a5ff22188d3e2621c9e1b622d2866f33e79770a
Paulo Acassio <pauloacassio2015@outlook.com>
2022-09-16 20:57:56 -0300

    User and Backend is working

---
+++ backend/src/main/java/dev/ifrs/Backend.java
@@ -0,0 +1,28 @@
+package dev.ifrs;
+
+import javax.annotation.security.RolesAllowed;
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import org.eclipse.microprofile.jwt.Claim;
+import org.eclipse.microprofile.jwt.Claims;
+
+@Path("/Backend")
+public class Backend {
+    //@Inject
+    //@Claim(standard = Claims.email)
+    //String email;
+
+    @GET
+    @Path("/{a}/{b}")
+    @Consumes(MediaType.TEXT_PLAIN)
+    @Produces(MediaType.TEXT_PLAIN)
+    @RolesAllowed({ "Admin" })
+    public int getSum(@PathParam("a") int a,@PathParam("b") int b){
+        return a+b;
+    }
+}
--- user/src/main/java/dev/ifrs/GreetingResource.java
+++ user/src/main/java/dev/ifrs/GreetingResource.java
@@ -1,16 +1,32 @@
 package dev.ifrs;
 
+import java.util.Arrays;
+import java.util.HashSet;
+
+import javax.annotation.security.PermitAll;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.SecurityContext;
 
-@Path("/hello")
+import org.eclipse.microprofile.jwt.Claims;
+
+import io.smallrye.jwt.build.Jwt;
+
+@Path("/Jwt")
 public class GreetingResource {
 
     @GET
+    @PermitAll
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello RESTEasy";
+    public String generate(@Context SecurityContext ctx) {
+        return Jwt.issuer("http://localhost:8080")
+                .upn("paulo.oliveira@dominio.com")
+                .groups(new HashSet<>(Arrays.asList("User", "Admin")))
+                .claim(Claims.full_name, "Paulo Oliveira")
+                .sign();
     }
+
 }
